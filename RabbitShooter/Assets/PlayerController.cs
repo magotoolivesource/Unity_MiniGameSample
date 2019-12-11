96973b79aa27fb926e17639fbbf5029fa4eaa76f
심화강좌용 <tool@gmail.com>
2019-12-11 10:35:54 +0900

    서바이벌 슈터 기본세팅 움직임 제어, 카매라 움직임

---
+++ RabbitShooter/Assets/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    public float MoveSpeed = 1f;
+
+    void UpdateMoveControl()
+    {
+        float xmove = Input.GetAxis("Horizontal");
+        float ymove = Input.GetAxis("Vertical");
+
+        Vector3 temppos = transform.position;
+        temppos.x += xmove * MoveSpeed;
+        temppos.z += ymove * MoveSpeed;
+
+        transform.position = temppos;
+
+    }
+
+
+    void Update()
+    {
+        UpdateMoveControl();
+
+
+    }
+}
---
+++ RabbitShooter/Assets/TargetFoller.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetFoller : MonoBehaviour
+{
+    public Transform TargetTrans = null;
+    public float SmoothVal = 0.3f;
+
+    Vector3 m_OffsetPos;// = new Vector3();
+    void Awake()
+    {
+        m_OffsetPos = transform.position - TargetTrans.position;
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        Vector3 tempos = TargetTrans.position + m_OffsetPos;
+        transform.position = Vector3.Lerp(transform.position
+            , tempos
+            , SmoothVal * Time.deltaTime);
+
+
+    }
+}
