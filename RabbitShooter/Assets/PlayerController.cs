0d903a9e6ac322591984678f73165763be7d2dd7
심화강좌용 <tool@gmail.com>
2019-12-11 11:25:11 +0900

    마우스 회전부분 추가

--- RabbitShooter/Assets/PlayerController.cs
+++ RabbitShooter/Assets/PlayerController.cs
@@ -26,9 +26,76 @@ public class PlayerController : MonoBehaviour
     }
 
 
+    public float RotSpeed = 1f;
+
+    [Header("[확인용]"), SerializeField]
+    private float m_TempVal = 0f;
+    [SerializeField]
+    private Vector2 m_TempMouse = new Vector2();
+    [SerializeField]
+    private Vector3 m_MousePosition = new Vector3();
+    [SerializeField]
+    private Vector3 m_PlayerTOScreenPosition = new Vector3();
+
+
+    private float m_RadianTODergger = (180 / Mathf.PI);
+
+
+    void UpdateRotation()
+    {
+        m_MousePosition = Input.mousePosition;
+        m_PlayerTOScreenPosition = 
+            Camera.main.WorldToScreenPoint(transform.position);
+
+
+
+        m_MousePosition = m_MousePosition - m_PlayerTOScreenPosition;
+
+
+        //Debug.Log("위치값 : " + m_MousePosition.ToString());
+
+
+        m_TempVal = Mathf.Atan2( -m_MousePosition.y, m_MousePosition.x );
+        m_TempVal = m_TempVal * Mathf.Rad2Deg;
+
+        transform.rotation = Quaternion.Euler(0f, m_TempVal, 0);
+
+
+
+
+
+
+        //if( Input.GetKey( KeyCode.Q ) )
+        //{
+        //    // 1도 회전
+        //    Quaternion temprot = transform.rotation;
+
+        //    Vector3 rot = temprot.eulerAngles;
+        //    rot.y += RotSpeed;
+        //    temprot = Quaternion.Euler(rot);
+
+        //    transform.rotation = temprot;
+        //}
+
+        //if (Input.GetKey(KeyCode.E))
+        //{
+        //    // 1도 회전
+        //    Quaternion temprot = transform.rotation;
+
+        //    Vector3 rot = temprot.eulerAngles;
+        //    rot.y -= RotSpeed;
+        //    temprot = Quaternion.Euler(rot);
+
+        //    transform.rotation = temprot;
+        //}
+
+
+    }
+
     void Update()
     {
         UpdateMoveControl();
+        UpdateRotation();
 
 
     }
