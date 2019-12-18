f30432076718b9dc1423bf5c1ed7d6e1ecf77421
심화강좌용 <tool@gmail.com>
2019-12-18 11:15:25 +0900

    ui 부분 적용하기

---
+++ CrashRoyal/Assets/_CrashRoyal/Script/ActorTableManager.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class ActorTableData
+{
+    public ActorTableData( int p_id)
+    {
+        m_ID = p_id;
+    }
+
+
+    [SerializeField]
+    private int m_ID = -1;
+    public int ID
+    {
+        get => m_ID;
+        protected set => m_ID = value;
+    }
+
+    // 데이터값
+    public string Name = "";
+    public int Cost = 1;
+
+    public AttackCom AttackComData = new AttackCom();
+
+    // UI 관련 데이터
+    public Sprite UICardSprite = null;
+
+
+    public Transform InGameModel = null;
+
+    
+}
+
+public class ActorTableManager : SingleTon<ActorTableManager>
+{
+    [SerializeField]
+    protected List<ActorTableData> m_ActorTableDataList = new List<ActorTableData>();
+
+    public ActorTableData GetActorID( int p_id )
+    {
+        foreach (var item in m_ActorTableDataList)
+        {
+            if(item.ID == p_id)
+            {
+                return item;
+            }
+        }
+
+        return null;
+    }
+
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ CrashRoyal/Assets/_CrashRoyal/Script/UICard.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+
+
+
+public class UICard : MonoBehaviour
+{
+    public int m_ActorID = -1;
+
+    ActorTableData m_LinkTableData = null;
+
+    // 실제 적용위한 값
+    public Image CardImage = null;
+    public Text CostLabel = null;
+    public Text CardLabel = null;
+
+    public void SetInitUICard(int p_id)
+    {
+        m_ActorID = p_id;
+
+        m_LinkTableData = ActorTableManager.GetI.GetActorID(m_ActorID);
+
+        CardImage.sprite = m_LinkTableData.UICardSprite;
+        CardLabel.text = m_LinkTableData.Name;
+        CostLabel.text = m_LinkTableData.Cost.ToString();
+    }
+
+
+    public void PointDownEvent( BaseEventData p_data )
+    {
+        PointerEventData eventdata = p_data as PointerEventData;
+        Debug.Log("클릭 : " + eventdata.position );
+    }
+    public void PointUpEvent(BaseEventData p_data)
+    {
+        PointerEventData eventdata = p_data as PointerEventData;
+
+        if( m_ISDrag )
+        {
+            Vector3 screenpos = eventdata.position;
+            //Input.mousePosition;
+
+            Ray ray = Camera.main.ScreenPointToRay(screenpos);
+            LayerMask layermask = LayerMask.GetMask("Land");
+            RaycastHit hitinfo;
+
+            if(Physics.Raycast(ray, out hitinfo, 100f, layermask))
+            {
+                Vector3 worldpos = hitinfo.point;
+
+                Debug.Log("위치값 : " + worldpos);
+
+                //GameObject.Instantiate();
+            }
+            
+
+
+        }
+
+        m_ISDrag = false;
+    }
+
+    bool m_ISDrag = false;
+    public void DragEvent(BaseEventData p_data)
+    {
+        PointerEventData eventdata = p_data as PointerEventData;
+        //BaseEventData
+
+        m_ISDrag = true;
+
+        Debug.Log("드래그 : " + eventdata.position);
+    }
+
+
+    void Start()
+    {
+        // 테스트 코드
+        SetInitUICard(m_ActorID);
+    }
+
+    void Update()
+    {
+        
+    }
+}
